f887f3ec1b646c23e93d5bad320ce471128f2bbe
Yoda Lee <lc85301@gmail.com>
2026-08-30 14:50:00 +0800

    Implement fft and fft_recursive

--- src/hqc_py/fft.py
+++ src/hqc_py/fft.py
@@ -75,6 +75,74 @@ class FFT:
         R1.extend(Q1)
         return R0, R1
 
+    def _fft_recursive(self, f: list[GF2m], f_coeffs: int, m: int, m_f: int, betas: list[GF2m]) -> list[GF2m]:
+        """Recursive FFT evaluation of a polynomial in GF(2^m)[x].
+
+        Args:
+            f: Polynomial coefficients. The input size is a power of 2.
+            f_coeffs: Number of coefficients in ``f`` (that is, deg(f) + 1).
+            m: Number of betas (the degree of the Galois field GF(2^m)).
+            m_f: Number of coefficients of f (degree + 1)
+            deltas: Precomputed values for the FFT.
+
+        returns:
+            A list of the evaluations of ``f`` at all subset sums of a basis.
+        """
+        # terminal condition
+        if m_f == 1:
+            w = [GF2m(0)] * (1 << m)
+            tmp = [GF2m(0)] * m
+            for i in range(m):
+                tmp[i] = betas[i] * f[1]
+
+            w[0] = f[0]
+            x = 1
+            for j in range(m):
+                for k in range(x):
+                    w[x + k] = w[k] + tmp[j]
+                x *= 2
+            return w
+
+        # Compute g
+        if betas[m - 1] != 1:
+            beta_m_pow = GF2m(1)
+            x = 1 << m_f
+            for i in range(1, x):
+                beta_m_pow = beta_m_pow * betas[m - 1]
+                f[i] = beta_m_pow * f[i]
+
+        # Step 3
+        f0, f1 = self.radix(f, m_f)
+
+        # Step 4
+        gammas = [GF2m(0)] * (GF2m.m - 2)
+        deltas = [GF2m(0)] * (GF2m.m - 2)
+        for i in range(m-1):
+            gammas[i] = betas[i] * betas[m - 1].inverse();
+            deltas[i] = gammas[i].square() + gammas[i]
+
+        gammas_sums = self.compute_subset_sums(gammas)
+
+        # Step 5
+        u = self._fft_recursive(f0, (f_coeffs + 1) // 2, m - 1, m_f - 1, deltas)
+
+        k = 1 << (m - 1)
+        w = [GF2m(0)] * (1 << m)
+        if (f_coeffs <= 3): # 3-coefficent polynomial f case: f1 is constant
+            w[0] = u[0]
+            w[k] = u[0] + f1[0]
+            for i in range(1, k):
+                w[i]     = u[i] + (gammas_sums[i] * f1[0])
+                w[k + i] = w[i] + f1[0]
+        else:
+            w[k:] = self._fft_recursive(f1, f_coeffs // 2, m - 1, m_f - 1, betas)
+            w[0] = u[0]
+            w[k] += u[0]
+            for i in range(1, k):
+                w[i]      = u[i] + (gammas_sums[i] * f1[0])
+                w[k + i] += w[i]
+        return w
+
     def fft(self, f: list[GF2m], f_coeffs: int) -> list[GF2m]:
         """Evaluate f on all field elements using an additive FFT algorithm.
 
@@ -88,6 +156,37 @@ class FFT:
             f_coeffs: Number of coefficients in `f`.
 
         Returns:
-            A list of the evaluations of `f` at all field elements.
+            A list with length of 2^m (256) in default cases
         """
-        raise NotImplementedError("additive FFT core is not implemented yet")
+        assert len(f) == 1 << self.n_fft
+
+        # 1. The betas and betas_sums are precomputed for the FFT.
+        # 2. beta_m = 1, nothing to do.
+        # 3. Radix the input f
+        f0, f1 = self.radix(f, self.n_fft)
+
+        # 4. Compute the gammas and deltas
+        deltas = [GF2m(0)] * (GF2m.m - 1)
+        for i in range(GF2m.m - 1):
+            deltas[i] = self.betas[i].square() + self.betas[i]
+
+        # 5. Recursive fft
+        u = self._fft_recursive(f0, (f_coeffs + 1) // 2, GF2m.m - 1, self.n_fft - 1, deltas)
+        v = self._fft_recursive(f1, f_coeffs // 2, GF2m.m - 1, self.n_fft - 1, deltas)
+
+        # check root
+        k = 1 << (GF2m.m - 1)
+        w = ([GF2m(0)] * k) + v
+
+        # Check if 0 is root
+        w[0] = u[0]
+
+        # Check if 1 is root
+        w[k] += u[0]
+
+        # Find other roots
+        for i in range(1, k):
+            w[i] = u[i] + self.betas_sums[i] * v[i]
+            w[k+i] += w[i]
+
+        return w
